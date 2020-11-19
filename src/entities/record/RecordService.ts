864ae94edc96a94ba0b153a5b32ac7edc5047cd2
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-19 21:34:27 +0100

    record state

---
+++ src/entities/record/RecordService.ts
@@ -0,0 +1,18 @@
+import { CreateRecordRequestModel } from "./models/CreateRecordRequestModel";
+import { Record } from "./models/Record";
+
+// TODO
+export class RecordService {
+    
+    Create(record: CreateRecordRequestModel) {
+        throw new Error("not implemented");
+    }
+
+    GetAll(): Array<Record> {
+        throw new Error("not implemented");
+    }
+
+    Get(id: string): Record {
+        throw new Error("not implemented");
+    }
+}
