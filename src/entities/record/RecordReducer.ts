429426c02b3fca69f2bdbe529033495fb11ce1de
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-19 21:33:58 +0100

    records reducer and actions

---
+++ src/entities/record/RecordReducer.ts
@@ -0,0 +1,11 @@
+import { RecordAction } from "./models/action/RecordAction";
+import { IRecordState } from "./state/IRecordState";
+
+// TODO
+export const recordReducer = (state: IRecordState, action: RecordAction): IRecordState => {
+    switch(action.type) {
+        
+        default:
+            return state
+    }
+}
