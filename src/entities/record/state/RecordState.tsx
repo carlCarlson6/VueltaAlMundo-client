76be708b89b53b3f33f485bc46c30720c9495559
carlCarlson6 <bullet.carlos@gmail.com>
2020-11-21 23:56:42 +0100

    misc

--- src/entities/record/state/RecordState.tsx
+++ src/entities/record/state/RecordState.tsx
@@ -5,15 +5,14 @@ import { RecordService } from '../RecordService';
 import { initialRecordState } from './InitialRecordState';
 
 export const RecordState = (props: any) => {
-    const [state, dispatch] = React.useReducer(recordReducer, initialRecordState)
-
-    const recordService: RecordService = new RecordService(dispatch);
+    const [state, dispatch] = React.useReducer(recordReducer, initialRecordState);
+    const service: RecordService = new RecordService(dispatch);
 
     return (
         <RecordContext.Provider
             value = {{
                 state,
-                service: recordService
+                service
             }}
         >
             {props.children}
